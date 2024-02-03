ee5cf700fe564e3514ebfb7b05c2399f1811107d
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-02-03 23:15:12 +0100

    adding important files of the Horca

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import {HangmanDrawing} from "./HangmanDrawing"
+import {HangmanWord}  from "./HangmanWord"
+import {Keyboard}  from "./Keyboard"
+import words from "./wordList.json"
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(()=> {
+
+    return words[Math.floor(Math.random() * words.length)]    //se utiliza este metodo para obetenr un numero aleatorio y sacar una palabra de la lista json
+
+  }) //this two parameters keep track of the word that is getting guessed and the setted word to guess
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
   
-  return  <h1>Hi</h1>
-  
+  return (
+    <div 
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
 }
 
 export default App
---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,6 @@
+//con esta funcion construimos el dibujo de la horca
+export function HangmanDrawing() {
+    return <div style={{position: "relative"}}> 
+
+    </div>
+}
