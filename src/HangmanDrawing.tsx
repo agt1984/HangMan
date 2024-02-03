35daeb3b139c64a00ab753cc92244015cd8ac2d5
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-02-03 23:18:51 +0100

    adding important files of the Horca

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,6 +1,6 @@
 //con esta funcion construimos el dibujo de la horca
 export function HangmanDrawing() {
     return <div style={{position: "relative"}}> 
-
+        <div style={{height: "10px", width: "250px", background: "black"}}/>
     </div>
 }
