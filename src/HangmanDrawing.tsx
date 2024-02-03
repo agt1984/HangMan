486ce318988954088946e9943df410c8b7500f74
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-02-03 23:33:04 +0100

    adding important styles modification to the HORCA

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,6 +1,49 @@
+const HEAD = (
+    <div
+        style={{
+            width: "50px",
+            height: "50px",
+            borderRadius: "100%",
+            border: "10px solid black",
+            position: "absolute",
+            top: "50px",
+            right: "-30px",
+        }}
+    />
+)
+
+
 //con esta funcion construimos el dibujo de la horca
 export function HangmanDrawing() {
     return <div style={{position: "relative"}}> 
+        {HEAD}
+        <div
+            style={{
+                height: "50px",
+                width: "10px",
+                background: "black",
+                marginLeft: "120px", 
+                position: "absolute",
+                top: 0,
+                right: 0,
+            }}   
+        />
+        <div
+            style={{
+                height: "10px",
+                width: "200px",
+                background: "black",
+                marginLeft: "120px", 
+            }}   
+        />
+        <div
+            style={{
+                height: "400px",
+                width: "10px",
+                background: "black",
+                marginLeft: "120px", 
+            }}   
+        />
         <div style={{height: "10px", width: "250px", background: "black"}}/>
     </div>
 }
