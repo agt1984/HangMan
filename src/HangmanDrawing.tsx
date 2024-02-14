640ee54a1afdd7fab8ee892103a4348e38f79a9d
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-02-14 20:27:17 +0100

    the body of the hangman is rendered

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -12,11 +12,89 @@ const HEAD = (
     />
 )
 
+const BODY = (
+    <div
+        style={{
+            width: "10px",
+            height: "100px",
+            background: "black",
+            position: "absolute",
+            top: "120px",
+            right: "0",
+        }}
+    />
+)
+
+const RIGTH_ARM = (
+    <div
+        style={{
+            width: "100px",
+            height: "10px",
+            background: "black",
+            position: "absolute",
+            top: "150px",
+            right: "-100px",
+            rotate: "-30deg",
+            transformOrigin: "left bottom",
+        }}
+    />
+)
+
+const LEFT_ARM = (
+    <div
+        style={{
+            width: "100px",
+            height: "10px",
+            background: "black",
+            position: "absolute",
+            top: "100px",
+            right: "-4px",
+            rotate: "30deg",
+            transformOrigin: "rigth bottom",
+        }}
+    />
+)
+
+const RIGTH_LEG = (
+    <div
+        style={{
+            width: "100px",
+            height: "10px",
+            background: "black",
+            position: "absolute",
+            top: "210px",
+            right: "-90px",
+            rotate: "60deg",
+            transformOrigin: "left bottom",
+        }}
+    />
+)
+
+const LEFT_LEG = (
+    <div
+        style={{
+            width: "100px",
+            height: "10px",
+            background: "black",
+            position: "absolute",
+            top: "295px",
+            right: "-50px",
+            rotate: "-60deg",
+            transformOrigin: "rigth bottom",
+        }}
+    />
+)
+
 
 //con esta funcion construimos el dibujo de la horca
 export function HangmanDrawing() {
     return <div style={{position: "relative"}}> 
         {HEAD}
+        {BODY}
+        {RIGTH_ARM}
+        {LEFT_ARM}
+        {RIGTH_LEG}
+        {LEFT_LEG}
         <div
             style={{
                 height: "50px",
